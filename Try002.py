de1093168a8de8249d8048d86092beebd3d01aee
吕艳朋 <coldhair@163.com>
2019-06-27 16:51:34 +0800

    控制语句学习

--- Try002.py
+++ Try002.py
@@ -1,11 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
-
 x1 = np.linspace(1, 10, 20)
 print(x1)
 y1 = x1 * x1 + 2
 fig = plt.figure()
 axes = fig.add_axes([0.1, 0.1, 0.9, 0.9])
 axes.plot(x1, y1, 'r')
-
 plt.show()
---
+++ Try003.py
@@ -0,0 +1,8 @@
+a=1
+while a<20:
+    if a%2==0:
+        print(a,'是偶数。')
+    else:
+        print(a,'是奇数。')
+    a+=1
+#循环语句的条件语句 https://www.runoob.com/python3/python3-conditional-statements.html
