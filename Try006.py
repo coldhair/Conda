dfb489da270c5e21c3f8c1eaa0d59407d858aaf4
Administrator <coldhair@163.com>
2019-06-27 21:23:32 +0800

    Python字典

---
+++ Try006.py
@@ -0,0 +1,13 @@
+# 字典
+dict={'name': 'Runoob', 'Age':7, 'class':2}
+print("dict['name']:", dict['name'])
+print("dict['Age']:", dict['Age'])
+dict['school']='SDU'
+print(dict)
+del dict['Age']
+print(dict)
+dict.clear()
+print('清空后的长度:',len(dict))
+print(dict)
+del dict
+print(dict)
