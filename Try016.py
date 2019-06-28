a3c6358ec369681a0599d85eacb1ab153bc14196
吕艳朋 <coldhair@163.com>
2019-06-28 17:25:05 +0800

    /函数练习

--- Try016.py
+++ Try016.py
@@ -9,3 +9,23 @@ mylist = [10, 20, 30]
 see = changeme(mylist)
 print('函数外取值：', see)
 print('函数外取值：', mylist)
+
+
+def printme(str):
+    # 打印任何传入的字符串
+    print(str)
+    return
+
+
+# 调用printme函数
+printme(str='菜鸟教程')
+
+# 默认参数的函数
+def dayinInfo(name,age=36):
+    print('名字：',name)
+    print('年龄：',age)
+    return
+#调用dayinInfo函数
+dayinInfo(age=50,name='吕艳朋')
+print('---------------')
+dayinInfo(name='吕艳风')
