6151d68d6ea1f14ca4d7b9c5ea28d7001d175606
Administrator <coldhair@163.com>
2019-06-28 11:49:54 +0800

    类作为迭代器

---
+++ Try012.py
@@ -0,0 +1,21 @@
+# 把一个类作为一个迭代器
+class MyNumbers:
+    def __iter__(self):
+        self.a=1
+        return self
+
+    def __next__(self):
+        x= self.a
+        self.a +=1
+        return x
+
+
+myclass = MyNumbers()
+myiter=iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
