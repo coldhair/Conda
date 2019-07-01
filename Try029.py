08d6e2347398140172a0f7ea4fab25cf4415eb4b
Administrator <coldhair@163.com>
2019-07-01 21:34:58 +0800

    列表推导式

--- Try029.py
+++ Try029.py
@@ -1,6 +1,13 @@
 # 数据结构 列表推导式
-vec=[2,4,6]
-a=[3*x for x in vec]
+vec = [2, 4, 6]
+a = [3 * x for x in vec]
 print(a)
-b= [[x,x**2] for x in vec]
+b = [[x, x ** 2] for x in vec]
 print(b)
+d = [3 * x for x in vec if x > 3]
+print(d)
+
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+c = [weapon.strip() for weapon in freshfruit]
+print(freshfruit)
+print(c)
