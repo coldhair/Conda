8ee8a8ff743a23eb82e900e0e691e49f58fd27ec
Administrator <coldhair@163.com>
2019-07-02 23:30:55 +0800

    turtle学习

---
+++ Try035.py
@@ -0,0 +1,12 @@
+# 绘制同心圆
+import turtle
+turtle.screensize(800,600, "green") # 画布的宽(单位像素), 高, 背景颜色。
+turtle.pensize(1)
+turtle.pencolor('red')
+turtle.speed(8)
+turtle.circle(10)
+turtle.circle(40)
+turtle.circle(80)
+turtle.circle(160)
+
+
