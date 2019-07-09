7a1bafa30b37563ac8255b4c5213b5ef8b0afa97
吕艳朋 <coldhair@163.com>
2019-07-09 11:32:46 +0800

    ok

--- Try060.py
+++
@@ -1,7 +0,0 @@
-from urllib import request
-with request.urlopen('https://httpbin.org/ip') as f:
-    data= f.read()
-    print('Status:',f.status,f.reason)
-    for k,v in f.getheaders():
-        print('{}:{}'.format(k,v))
-    print('Data:',data.decode('utf-8'))
