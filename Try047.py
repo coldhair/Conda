7aa11215f5b32489d5ecfc2e709ae70fae818c61
coldhair <coldhair@163.com>
2020-02-11 21:30:09 +0800

    Update Try047.py
    乘法表，数字两位数格式化输出，使其对齐。{2: <2d}

--- Try047.py
+++ Try047.py
@@ -6,5 +6,5 @@ Author: 吕艳朋
 """
 for i in range(1, 10):
     for j in range(1, i + 1):
-        print('{1}×{0}={2}'.format(i, j, i * j), end=' ')
+        print('{1}×{0}={2: <2d}'.format(i, j, i * j), end=' ')
     print()
