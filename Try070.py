8fdafd3c4af63d068f8aea9595b136570a9b0001
coldhair <coldhair@163.com>
2020-03-15 16:29:45 +0800

    增加解码

--- Try070.py
+++ Try070.py
@@ -1,3 +1,3 @@
 import urllib.request
-with urllib.request.urlopen('http://www.piyas.com') as f:
-    print(f.read(3000))
+with urllib.request.urlopen('http://www.piyas.com/a/2693.html') as f:
+    print(f.read(30000).decode('utf8')) # 加上网页的编码，不然会显示乱码
